6f1cffa770658a3ce683c63c6148e80f36d3a873
NimaiDev <nimaic.dev@gmail.com>
2025-02-23 00:43:00 +0530

    Added key binding for file explorer

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+--
+--
+-- Keymaps for TreeSitter
+vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", { desc = "Toggle File Explorer" })
